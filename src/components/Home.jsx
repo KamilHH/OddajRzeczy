90c76873c3f98521bd8114aa4c93314ef42e6a51
KamilHH <wlad.bart12@gmail.com>
2022-05-10 18:52:03 +0200

    home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import HomeHeader from "./HomeHeader";
+import HomeThreeColumns from "./HomeThreeColumns";
+import HomeSteps from "./HomeSteps";
+import HomeAbout from "./HomeAbout";
+import HomeWho from "./HomeWho";
+import HomeContact from "./HomeContact";
+
+const Home = () => {
+    return (
+        <div>
+            <HomeHeader/>
+            <HomeThreeColumns/>
+            <HomeSteps/>
+            <HomeAbout/>
+            <HomeWho/>
+            <HomeContact/>
+        </div>
+    );
+};
+
+export default Home;
