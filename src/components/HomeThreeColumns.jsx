9293c2ceb8dbfa19a88de44e87624a51c3a0e44a
KamilHH <wlad.bart12@gmail.com>
2022-05-13 18:13:54 +0200

    Three columns section component

--- src/components/HomeThreeColumns.jsx
+++ src/components/HomeThreeColumns.jsx
@@ -2,7 +2,34 @@ import React from 'react';
 
 const HomeThreeColumns = () => {
     return (
-        <div>
+        <div className="columns" id="3columns">
+            <div className="columns__item">
+                <h1 className="columns__title">10</h1>
+                <h2 className="columns__description">ODDANYCH WORKÓW</h2>
+                <p className="columns__content">
+                   Lorem ipsum dolor sit amet, consectetur adipisc
+                    Pellentesque vel enim a elit viverra elementuma.
+                    Aliquam erat volutpat.
+                </p>
+            </div>
+            <div className="columns__item">
+                <h1 className="columns__title">5</h1>
+                <h2 className="columns__description">WSPARTYCH AKCJI</h2>
+                <p className="columns__content">
+                     Lorem ipsum dolor sit amet, consectetur adipisc
+                    Pellentesque vel enim a elit viverra elementuma.
+                    Aliquam erat volutpat.
+                </p>
+            </div>
+            <div className="columns__item">
+                <h1 className="columns__title">7</h1>
+                <h2 className="columns__description">ZORGANIZOWANYCH ZBIÓREK</h2>
+                <p className="columns__content">
+                     Lorem ipsum dolor sit amet, consectetur adipisc
+                    Pellentesque vel enim a elit viverra elementuma.
+                    Aliquam erat volutpat.
+                </p>
+            </div>
 
         </div>
     );
