270d2f75697d12cdff4b311e31c0e516b91c4095
KamilHH <wlad.bart12@gmail.com>
2022-05-13 18:14:20 +0200

    About us  section component

--- src/components/HomeAbout.jsx
+++ src/components/HomeAbout.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import Decoration from '../assets/Decoration.svg'
+import Signature from '../assets/Signature.svg'
+import Photo from '../assets/People.jpg'
+
 
 const HomeAbout = () => {
     return (
-        <div>
-
+        <div className="about" id="about">
+            <div className="about__left-column">
+                <h1 className="about__title">O nas</h1>
+                <img src={Decoration} alt="" className="about__decor"/>
+                <p className="about__content">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aperiam doloremque est magni
+                    maxime nesciunt nobis numquam quidem tenetur vel.
+                </p>
+                <img src={Signature} alt="" className="about__signature"/>
+            </div>
+            <div className="about__right-column">
+                <img src={Photo} alt="" className="about__img"/>
+            </div>
         </div>
     );
 };
