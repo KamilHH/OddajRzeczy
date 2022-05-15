a56ed2994be937a51fac96d73da15c2f4df1bd90
KamilHH <wlad.bart12@gmail.com>
2022-05-15 16:42:14 +0200

    pagination conditional rendering

--- src/components/Foundation/Pagination.jsx
+++ src/components/Foundation/Pagination.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 
-const Pagination = ({postsPerPage, totalPosts, paginate}) => {
+const Pagination = ({postsPerPage, totalPosts, paginate, active}) => {
     const pageNumbers = [];
 
     for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
         pageNumbers.push(i)
     }
-    return (
-        <div className="foundation__pagination">
-            {pageNumbers.map(number => (
-                <button onClick={() => paginate(number)} key={number}
-                        className="button button button--small">{number}</button>
-            ))}
-        </div>
-    )
-
+    if (pageNumbers.length > 1) {
+        return (
+            <div className="foundation__pagination">
+                {pageNumbers.map(number => (
+                    <button
+                        key={number}
+                        className="button button--small"
+                        onClick={() => paginate(number)}
+                    >{
+                        number}
+                    </button>
+                ))}
+            </div>
+        )
+    }
 };
+
 
 export default Pagination;
