6eda86a2c25e3a70848b1c88e82a493d06d0e267
KamilHH <wlad.bart12@gmail.com>
2022-05-15 17:26:40 +0200

    cleanup

--- src/components/HomeContact.jsx
+++ src/components/HomeContact.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import Photo from "../assets/Background-Contact-Form.jpg"
+
 const HomeContact = () => {
     return (
         <div className="contact" id="contact">
+            <div className="contact__form"></div>
         </div>
     );
 };
