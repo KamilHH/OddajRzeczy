a9b017bdb3d28ff4b9de258c49bfa892974383be
KamilHH <wlad.bart12@gmail.com>
2022-05-16 14:18:54 +0200

    fragment

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import {Route, Routes} from "react-router-dom";
 
 const App = () => {
     return (
-        <div>
+        <>
             <Routes>
                 <Route path="/" element={<Home />}/>
             </Routes>
-        </div>
+        </>
     );
 };
 
