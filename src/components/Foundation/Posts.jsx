dd755b6f6926d3df8652197b655dd02c1fb346d7
KamilHH <wlad.bart12@gmail.com>
2022-05-18 19:17:40 +0200

    posts component

--- src/components/Foundation/Posts.jsx
+++ src/components/Foundation/Posts.jsx
@@ -5,16 +5,16 @@ const Posts = ({data}) => {
         <>
             {data.map(e => {
                 return (
-                    <div key={e.id} className="foundation__box">
-                        <div className="foundation__box-container">
-                            <div className="foundation__box-item">
-                                <span className="foundation__title">{e.title}</span>
-                                <span className="foundation__subtitle">{e.subtitle}</span>
+                        <div key={e.id} className="foundation__box">
+                            <div className="foundation__box-container">
+                                <div className="foundation__box-item">
+                                    <span className="foundation__title">{e.title}</span>
+                                    <span className="foundation__subtitle">{e.subtitle}</span>
+                                </div>
+                                <span
+                                    className="foundation__description">{e.description}</span>
                             </div>
-                            <span
-                                className="foundation__description">{e.description}</span>
                         </div>
-                    </div>
                 )
             })}
         </>
